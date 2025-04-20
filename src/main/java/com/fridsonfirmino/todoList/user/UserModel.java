952b3d72f2c9365ab86ef3fc4948a9826ba83c0b
FridsoFirmino <fridsoncmf@gmail.com>
2025-04-20 13:58:23 +0100

    fix(user): correct User model configuration to connect with PostgreSQL

--- src/main/java/com/fridsonfirmino/todoList/user/UserModel.java
+++ src/main/java/com/fridsonfirmino/todoList/user/UserModel.java
@@ -1,9 +1,30 @@
 package com.fridsonfirmino.todoList.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "users")
 public class UserModel {
+    
+    @Id 
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
     private String name;
     private String username;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
 
     public String getName() {
         return name;
@@ -27,5 +48,11 @@ public class UserModel {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
