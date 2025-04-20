dfd5cb82a2237826a890e125ee3626a2303986fc
FridsoFirmino <fridsoncmf@gmail.com>
2025-04-20 15:26:15 +0100

    feat(user): add password encryption when saving User

--- src/main/java/com/fridsonfirmino/todoList/user/UserController.java
+++ src/main/java/com/fridsonfirmino/todoList/user/UserController.java
@@ -1,10 +1,13 @@
 package com.fridsonfirmino.todoList.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 
 @RestController
@@ -22,10 +25,17 @@ public class UserController {
     private IUserRepository userRepository;
     
     @PostMapping("/create")
-    public UserModel create(@RequestBody UserModel userModel){
-        // System.out.println(userModel.getUsername());
-        this.userRepository.save(userModel);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null){
+            // 400 Bad Request
+            return ResponseEntity.status(400).body("User already exists");
+        }
 
-        return userModel;
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashred);
+        var userCreated = this.userRepository.save(userModel);
+        // 201 Created
+        return ResponseEntity.status(201).body(userCreated);
     }
 }
